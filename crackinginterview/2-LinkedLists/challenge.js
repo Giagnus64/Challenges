7c241270da2d926322d9ee2918a8b81db3f72dc2
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-11-21 22:09:11 -0500

    Added helper functions to linked list

--- crackinginterview/2-LinkedLists/challenge.js
+++ crackinginterview/2-LinkedLists/challenge.js
@@ -17,16 +17,72 @@ class LinkedList {
         this.size = 5; 
     } 
   
-    // functions to be implemented 
-    // add(element) 
+     
+    //adds an element at the end of the list
+    add(element){
+        const newNode = new Node(element);
+
+        if (!this.head){
+            this.head = newNode
+        } else{
+            let current = this.head
+            while(current.next){
+                current = current.next
+            }
+            current.next = newNode
+        }
+
+        this.size++;
+    }
+
     // insertAt(element, location) 
+    insertAt(element, location){
+
+        if(location < 0 || location > this.size){
+            return false;
+        }
+        const newNode = new Node(element);
+        let counter = 0
+        let current = this.head;
+
+        if(location === 0){
+           newNode.next = this.head
+           this.head = newNode 
+        } else {
+            while(!counter + 1 == location){
+                current = current.next
+                counter++;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        this.size++;
+        return newNode;
+    }
     // removeFrom(location) 
     // removeElement(element) 
   
-    // Helper Methods 
-    // isEmpty 
-    // size_Of_List 
-    // PrintList 
+    //helpers
+    isEmpty(){
+        if(this.size > 0){
+            return false
+        }
+        return true
+    }
+    
+    sizeOf() {
+        return this.size
+    }
+    
+    printList(){
+        let current = this.head;
+        while (current.next) {
+          console.log(current);
+          current = current.next;
+        }
+        console.log(current)
+    }
+
 } 
 
 const node1 = new Node("a")
@@ -41,4 +97,7 @@ node2.next = node3
 node3.next = node4
 node4.next = node5
 
-console.log(testList);
+
+
+
+//console.log(testList);
