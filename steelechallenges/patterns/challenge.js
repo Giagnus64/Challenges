ee4695df61677fb0ec7aeef283449c01d5cc5cc7
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-12-02 20:18:38 -0500

    Solved next multiple pointer problem

--- steelechallenges/patterns/challenge.js
+++ steelechallenges/patterns/challenge.js
@@ -1,7 +1,6 @@
+// *********** Frequency Tables *************
+
 //Anagram challenge
-
-
-
 const validAnagram = (str1, str2) => {
     if(str1.length !== str2.length){
         return false;
@@ -47,4 +46,41 @@ const sameFrequency = (num1, num2) => {
     }
     return true;
 
+}
+
+// ************** Multiple Pointers ************************
+
+
+//count unique values in sorted array
+const countUniqueValues = (arr) => {
+    let i = 0;
+    let j = 1;
+    while(j < arr.length) {
+        if(arr[i] === arr[j]){
+            j++;
+        }else{
+            i++;
+            arr.splice(i, 0, arr[j]);
+            j++;
+        }
+    }
+    return i + 1
+}
+
+//check duplicate arguments
+const areThereDuplicates = (...args) =>{
+    let i = 0;
+    let j = 1;
+    while (i < args.length - 1) {
+        if (args[i] == args[j]) {
+            return true;
+        }
+        if (j == args.length - 1) {
+            i++;
+            j = i + 1;
+        } else {
+            j++;
+        }
+    }
+    return false;
 }
