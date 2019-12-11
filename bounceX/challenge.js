1e8fbb62c9d687b5bb40bd1f94e3c1602b0aece2
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-12-10 23:23:48 -0500

    Finished 1st half of marmot challenge

--- bounceX/challenge.js
+++ bounceX/challenge.js
@@ -10,3 +10,19 @@ miniCartImages = miniCart.querySelectorAll('.mini-cart-image a');
 miniCartImages.forEach((anchorTag) => {
     cartObject.cartImages.push(anchorTag.href)
 });
+
+function getPercentScrolled(){
+    return ((window.pageYOffset + window.innerHeight) / (document.body.scrollHeight)) * 100
+}
+
+function displayCart(){
+    if(getPercentScrolled() >= 90){
+        console.log("last 10!")
+    }
+}
+document.body.addEventListener('scroll', displayCart)
+
+//design a modal that pops up
+//handle multiple scroll events firing
+//obscure the background
+//get cart link and place in object or variable
