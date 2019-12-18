fcca745382572bda0f5f5b9643b0be43c84ad7d0
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-12-18 15:17:12 -0500

    Pushed searches file

--- steelechallenges/searches/challenge.js
+++ steelechallenges/searches/challenge.js
@@ -1,1 +1,14 @@
 //Linear Search
+
+function linearSearch(arr, ele){
+    let i = 0;
+    while(i < arr.length){
+        if(arr[i] === ele) return i;
+        i++;
+    }
+    return -1;
+}
+
+function binarySearch(arr, ele){
+    let lesser = 0;
+}
