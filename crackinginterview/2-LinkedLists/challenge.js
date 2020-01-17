8d3301a75f52590d4e5edaab130db1df4f3e4a7d
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-01-17 14:22:11 -0500

    Implemented remove from and reverse List in singly

--- crackinginterview/2-LinkedLists/challenge.js
+++ crackinginterview/2-LinkedLists/challenge.js
@@ -1,98 +1,5 @@
-// User defined class node
-
-
-class Node {
-  // constructor
-  constructor(element) {
-    this.element = element;
-    this.next = null;
-  }
-}
-
-// linkedlist class 
-class LinkedList { 
-    constructor(size) 
-    { 
-        this.head = null; 
-        this.size = size; 
-    } 
-  
-     
-    //adds an element at the end of the list
-    add(element){
-        const newNode = new Node(element);
-
-        if (!this.head){
-            this.head = newNode
-        } else{
-            let current = this.head
-            while(current.next){
-                current = current.next
-            }
-            current.next = newNode
-        }
-
-        this.size++;
-    }
-
-    // insertAt(element, location) 
-    insertAt(element, location){
-
-        if(location < 0 || location > this.size){
-            return false;
-        }
-        const newNode = new Node(element);
-        let counter = 0
-        let current = this.head;
-
-        if(location === 0){
-           newNode.next = this.head
-           this.head = newNode 
-        } else {
-            while(counter + 1 !== location){
-                current = current.next
-                counter++;
-            }
-            newNode.next = current.next;
-            current.next = newNode;
-        }
-        this.size++;
-        return newNode;
-    }
-    // removeFrom(location) 
-    removeFrom(location){
-        //location is an INDEX, starting from 0
-        let counter = 0;
-        let current = this.head;
-        if(location === 0){
-            this.head = this.head.next
-        } else{
-            while (counter + 1 !== location){
-                current = current.next
-                counter++;
-                console.log(current);
-            }
-            current.next = current.next.next
-        }
-        this.size--;
-
-    }
-    // removeElement(element) 
-    removeElement = (element) => {
-        let current = this.head;
-        if(current.element === element){
-            this.head = current.next
-        } else {
-            while (current.next.element !== element) {
-                    current = current.next
-                    counter++;
-                }
-                current.next = current.next.next
-            }
-            this.size--;
-    }
-    
-    returnKthToLast(number) {
+// TODO - Refactor bottom code into properly made linked list
+returnKthToLast(number) {
         if (number > this.size) {
             return false
         }
@@ -123,51 +30,14 @@ class LinkedList {
     //         }
     //     }
     // }
-    //helpers
-    isEmpty(){
-        if(this.size > 0){
-            return false
-        }
-        return true
-    }
-    
-    sizeOf() {
-        return this.size
-    }
-    
-    printList(){
-        let current = this.head;
-        while (current.next) {
-          console.log(current);
-          current = current.next;
-        }
-        console.log(current)
-    }
 
 } 
 
-// const node1 = new Node("a")
-// const node2 = new Node("b")
-// const node3 = new Node("c")
-// const node4 = new Node("b")
-// const node5 = new Node("a")
-// const testList = new LinkedList(5)
-// testList.head = node1;
-// node1.next = node2
-// node2.next = node3
-// node3.next = node4
-// node4.next = node5
 
 
 
 
-//testList.removeElement('a');
-//testList.insertAt('test', 3);
-// console.log(testList);
-// testList.printList();
-//testList.removeDups();
-// console.log(testList.returnKthToLast(3));
-// testList.printList();
+
 
 
 // Remove Dups: Write code to remove duplicates from an unsorted linked list.
@@ -295,3 +165,218 @@ sum2Lists(testList1, testList2).printList();
 // Suppose the digits are stored in forward order.Repeat the above problem.EXAMPLE
 // Input: (6 -> 1 -> 7) + (2 -> 9 -> 5).Thatis, 617 + 295, Output: 9 -> 1 -> 2, Thatis, 912.
 // Hints: #7, #30, #71 #95, #109
+
+//FINALIZED IMPLEMENTATION OF SINGLY LINKED LIST
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// linkedlist class
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  //adds an val at the end of the list
+  push(val) {
+    //make a new Node
+    const newNode = new Node(val);
+    //if list is empty, make node head and tail
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      //otherwise add to end of list
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    //add 1 to length
+    this.length++;
+    //return list
+    return this;
+  }
+  //remove node from end of list
+  pop() {
+    //if list is empty (has no tail) return false
+    if (!this.tail) {
+      return false;
+    }
+    //get removed Node
+    const poppedNode = this.tail;
+    // if list has more than 1 node
+    if (this.head !== this.tail) {
+      //find new tail
+      const newTail = this.getNodeAtIndex(this.length - 2);
+      //remove newtail's reference to poppedNode
+      newTail.next = null;
+      //make it new tail
+      this.tail = newTail;
+      //otherwise establish list is empty
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+    //subtract 1 from length
+    this.length--;
+    //return poppedNode
+    return poppedNode;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    //if list is empty
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      // add current head pointer to new head(new node), and make new node new head
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    //add 1 to length
+    this.length++;
+
+    return this;
+  }
+  //remove first item in list
+  shift() {
+    //if list is empty return false
+    if (!this.head) return false;
+    //get shifted node
+    const shiftedNode = this.head;
+    //get new head (could be NULL if list is length 1)
+    const newHead = this.head.next;
+    //if newHead is null, reassign tail to newHead(null)
+    if (!newHead) {
+      this.tail = newHead;
+    }
+    //assign new head
+    this.head = newHead;
+    //remove 1 from length
+    this.length--;
+    //return shiftednode
+    return shiftedNode;
+  }
+  //return node at given index
+  getNodeAtIndex(index) {
+    //if index is not within list return null
+    if (index >= this.length || index < 0) return null;
+    //iterate through nodes until finding the one at index
+    let currentNode = this.head;
+    let currentIndex = 0;
+    while (currentIndex !== index) {
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+    return currentNode;
+  }
+  //change the node at the given index
+  setNodeAtIndex(val, index) {
+    //find the node using already built method
+    const foundNode = this.getNodeAtIndex(index);
+    //if the node is found update and return
+    if (foundNode) {
+      foundNode.value = val;
+      return foundNode;
+    }
+    //else return null
+    return null;
+  }
+  //insert a new node at the index with the given value
+  insertAt(index, val) {
+    //if index not valid return false
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    //if index is zero use already built unshift
+    if (index === 0) {
+      return this.unshift(val);
+    }
+    //if index is at end of list, use already built push
+    if (index === this.length) {
+      return this.push(val);
+    }
+    // make a new node, find the nodes before and after it, make before's next the new node, and the newNode's next the after
+    const newNode = new Node(val);
+    const after = this.getNodeAtIndex(index);
+    const before = this.getNodeAtIndex(index - 1);
+    newNode.next = after;
+    before.next = newNode;
+
+    this.length++;
+    return this;
+  }
+
+  // remove node from index
+  removeFrom(index) {
+    //if index is invalid return false
+    if (index < 0 || index >= this.length) {
+      return false;
+    }
+    //if index is at beginning use already built shift
+    if (index === 0) return this.shift();
+    //if index is at end use already built pop
+    if (index === this.length - 1) return this.pop();
+    //get before and after, set before's next to after and remove after's reference from removed Node
+    const before = this.getNodeAtIndex(index - 1);
+    const removedNode = this.getNodeAtIndex(index);
+    before.next = removedNode.next;
+    removedNode.next = null;
+
+    this.length--;
+    return removedNode;
+  }
+  // removeval(val)
+  removeVal(val) {
+    let current = this.head;
+    if (current.val === val) {
+      this.head = current.next;
+    } else {
+      while (current.next.val !== val) {
+        current = current.next;
+        counter++;
+      }
+      current.next = current.next.next;
+    }
+    this.length--;
+  }
+
+  //helpers
+  isEmpty() {
+    if (this.length > 0) {
+      return false;
+    }
+    return true;
+  }
+
+  lengthOf() {
+    return this.length;
+  }
+
+  printList() {
+    let current = this.head;
+    while (current.next) {
+      console.log(current);
+      current = current.next;
+    }
+    console.log(current);
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push("a");
+list.push("b");
+list.push("c");
+list.push("d");
+
+console.log(list.removeFrom(0));
+console.log(list.removeFrom(1));
+list.printList();
+console.log(list.removeFrom(1));
+
+list.printList();
--- steelechallenges/linkedlists/challenge.js
+++ steelechallenges/linkedlists/challenge.js
@@ -21,172 +21,169 @@ class SinglyLinkedList {
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
-    //otherwise add to end of list
+      //otherwise add to end of list
     } else {
       this.tail.next = newNode;
-      this.tail = newNode
+      this.tail = newNode;
     }
     //add 1 to length
     this.length++;
     //return list
-    return this
+    return this;
   }
+
   //remove node from end of list
   pop() {
-      //if list is empty (has no tail) return false
-      if(!this.tail){
-          return false
-      }
-      //get removed Node
-      const poppedNode = this.tail;
-      // if list has more than 1 node
-      if(this.head !== this.tail){
-        //find new tail
-        const newTail = this.findAtIndex(this.length - 2);
-        //remove newtail's reference to poppedNode
-        newTail.next = null;
-        //make it new tail
-        this.tail = newTail;
-        //otherwise establish list is empty
-      } else {
-        this.head = null;
-        this.tail = null;
-      }
-      //subtract 1 from length
-      this.length--;
-      //return poppedNode
-      return poppedNode
+    //if list is empty (has no tail) return false
+    if (!this.tail) {
+      return false;
+    }
+    //get removed Node
+    const poppedNode = this.tail;
+    // if list has more than 1 node
+    if (this.head !== this.tail) {
+      //find new tail
+      const newTail = this.getNodeAtIndex(this.length - 2);
+      //remove newtail's reference to poppedNode
+      newTail.next = null;
+      //make it new tail
+      this.tail = newTail;
+      //otherwise establish list is empty
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+    //subtract 1 from length
+    this.length--;
+    //return poppedNode
+    return poppedNode;
   }
 
-  unshift(val){
-      const newNode = new Node(val)
-      //if list is empty
-      if(!this.head){
-        this.head = newNode;
-        this.tail = newNode;
+  unshift(val) {
+    const newNode = new Node(val);
+    //if list is empty
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
       // add current head pointer to new head(new node), and make new node new head
-      } else {
-        newNode.next = this.head;
-        this.head = newNode;
-      }
-      //add 1 to length
-      this.length++;
-      
-      return this;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    //add 1 to length
+    this.length++;
 
+    return this;
   }
+
   //remove first item in list
-  shift(){
-      //if list is empty return false
-      if(!this.head) return false;
-      //get shifted node
-      const shiftedNode = this.head;
-      //get new head (could be NULL if list is length 1)
-      const newHead = this.head.next;
-      //if newHead is null, reassign tail to newHead(null)
-      if(!newHead){
-          this.tail = newHead;
-      }
-      //assign new head
-      this.head = newHead;
-      //remove 1 from length
-      this.length--;
-      //return shiftednode
-      return shiftedNode;
+  shift() {
+    //if list is empty return false
+    if (!this.head) return false;
+    //get shifted node
+    const shiftedNode = this.head;
+    //get new head (could be NULL if list is length 1)
+    const newHead = this.head.next;
+    //if newHead is null, reassign tail to newHead(null)
+    if (!newHead) {
+      this.tail = newHead;
+    }
+    //assign new head
+    this.head = newHead;
+    //remove 1 from length
+    this.length--;
+    //return shiftednode
+    return shiftedNode;
   }
 
-  insertAt(val, index) {
+  //return node at given index
+  getNodeAtIndex(index) {
+    //if index is not within list return null
+    if (index >= this.length || index < 0) return null;
+    //iterate through nodes until finding the one at index
+    let currentNode = this.head;
+    let currentIndex = 0;
+    while (currentIndex !== index) {
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+    return currentNode;
+  }
+
+  //change the node at the given index
+  setNodeAtIndex(val, index) {
+    //find the node using already built method
+    const foundNode = this.getNodeAtIndex(index);
+    //if the node is found update and return
+    if (foundNode) {
+      foundNode.value = val;
+      return foundNode;
+    }
+    //else return null
+    return null;
+  }
+
+  //insert a new node at the index with the given value
+  insertAt(index, val) {
+    //if index not valid return false
     if (index < 0 || index > this.length) {
       return false;
     }
+    //if index is zero use already built unshift
+    if (index === 0) {
+      return this.unshift(val);
+    }
+    //if index is at end of list, use already built push
+    if (index === this.length) {
+      return this.push(val);
+    }
+    // make a new node, find the nodes before and after it, make before's next the new node, and the newNode's next the after
     const newNode = new Node(val);
-    let counter = 0;
-    let current = this.head;
+    const after = this.getNodeAtIndex(index);
+    const before = this.getNodeAtIndex(index - 1);
+    newNode.next = after;
+    before.next = newNode;
 
-    if (location === 0) {
-      newNode.next = this.head;
-      this.head = newNode;
-    } else {
-      while (counter + 1 !== location) {
-        current = current.next;
-        counter++;
-      }
-      newNode.next = current.next;
-      current.next = newNode;
-    }
     this.length++;
-    return newNode;
+    return this;
   }
-  // removeFrom(location)
-  removeFrom(location) {
-    //location is an INDEX, starting from 0
-    let counter = 0;
-    let current = this.head;
-    if (location === 0) {
-      this.head = this.head.next;
-    } else {
-      while (counter + 1 !== location) {
-        current = current.next;
-        counter++;
-        console.log(current);
-      }
-      current.next = current.next.next;
-    }
-    this.length--;
-  }
-  // removeval(val)
-  removeVal(val){
-    let current = this.head;
-    if (current.val === val) {
-      this.head = current.next;
-    } else {
-      while (current.next.val !== val) {
-        current = current.next;
-        counter++;
-      }
-      current.next = current.next.next;
-    }
-    this.length--;
-  };
 
-  returnKthToLast(number) {
-    if (number > this.length) {
+  // remove node from index
+  removeFrom(index) {
+    //if index is invalid return false
+    if (index < 0 || index >= this.length) {
       return false;
     }
-    const nodeNum = this.length - number;
-    let counter = 0;
+    //if index is at beginning use already built shift
+    if (index === 0) return this.shift();
+    //if index is at end use already built pop
+    if (index === this.length - 1) return this.pop();
+    //get before and after, set before's next to after and remove after's reference from removed Node
+    const before = this.getNodeAtIndex(index - 1);
+    const removedNode = this.getNodeAtIndex(index);
+    before.next = removedNode.next;
+    removedNode.next = null;
+
+    this.length--;
+    return removedNode;
+  }
+
+  reverseList() {
+    const newHead = this.tail;
+    const newTail = this.head;
+    let prev = null;
+    let next = null;
     let current = this.head;
-    while (nodeNum !== counter) {
-      current = current.next;
-      counter++;
-      //console.log(current)
+    while (current) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
     }
-    return current;
+    this.head = newHead;
+    this.tail = newTail;
+    return this;
   }
-
-  //helpers
-  isEmpty() {
-    if (this.length > 0) {
-      return false;
-    }
-    return true;
-  }
-
-  lengthOf() {
-    return this.length;
-  }
-
-  findAtIndex(index){
-      if(index >= this.length || index < 0) return null;
-      let currentNode = this.head
-      let currentIndex = 0;
-      while(currentIndex !== index){
-          currentNode = currentNode.next
-          currentIndex++;
-      }
-      return currentNode
-  }
-
   printList() {
     let current = this.head;
     while (current.next) {
@@ -195,18 +192,18 @@ class SinglyLinkedList {
     }
     console.log(current);
   }
-} 
+}
 
 let list = new SinglyLinkedList();
-//console.log(list);
-// list.push(1);
-// // console.log(list.pop())
-// // console.log(list);
-// list.push(2);
-// list.push(3);
-console.log(list.unshift(0));
+// list.push("a");
+// list.push("b");
+// list.push("c");
+// list.push("d");
 
 
+
+// console.log(list.reverseList());
+// list.printList();
 
 
 // //DOUBLY LINKED
@@ -378,7 +375,6 @@ console.log(list.unshift(0));
 //       return currentNode;
 //   }
 // }
-
 
 // console.log(list.removeAtIndex(1));
 // list.push(3);
