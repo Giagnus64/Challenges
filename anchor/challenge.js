b3ce288993f4e71334943c9a293ae7677d8bc91f
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-01-31 13:19:06 -0500

    Formatted code

--- anchor/challenge.js
+++ anchor/challenge.js
@@ -1,34 +1,37 @@
-const testStr = "We're an independent division within Spotify, with the power to move fast, plus the resources of the world's most forward-thinking audio company. Come help us build the future of podcasting! If you want to jumpstart the process of talking to us about this role, here’s a little challenge: write a program that outputs the largest unique set of characters that can be removed from this paragraph without letting its length drop below 50."
+const testStr =
+  "We're an independent division within Spotify, with the power to move fast, plus the resources of the world's most forward-thinking audio company. Come help us build the future of podcasting! If you want to jumpstart the process of talking to us about this role, here’s a little challenge: write a program that outputs the largest unique set of characters that can be removed from this paragraph without letting its length drop below 50.";
 
-const stringToArray = (str) => {
-    //split array into array of characters
-    const letterArr = str.split("");
-    //remove spaces - !! NOTE: I'm not counting spaces as characters
-    return letterArr.filter((character) => {return character !== ' '});
-}
+const stringToArray = str => {
+  //split array into array of characters
+  const letterArr = str.split("");
+  //remove spaces - !! NOTE: I'm not counting spaces as characters
+  return letterArr.filter(character => {
+    return character !== " ";
+  });
+};
 
-const createHashTable = (arr) => {
-    //create a hash table with the character and number of times it appears in the paragraph
-    const hashTable = {};
-    arr.forEach((character) => {
-        if(!hashTable[character]){
-            hashTable[character] = 1;
-        } else {
-            hashTable[character] += 1;
-        }
-    })
-    return hashTable;
-}
+const createHashTable = arr => {
+  //create a hash table with the character and number of times it appears in the paragraph
+  const hashTable = {};
+  arr.forEach(character => {
+    if (!hashTable[character]) {
+      hashTable[character] = 1;
+    } else {
+      hashTable[character] += 1;
+    }
+  });
+  return hashTable;
+};
 
 //split object into array of objects by character
-const createObjectArray = (hashTable) => {
-    const characterObjectArray = [];
-    
-    for (let key in hashTable){
-        characterObjectArray.push({character: key, num: hashTable[key]})
-    }
-    return characterObjectArray;
-}
+const createObjectArray = hashTable => {
+  const characterObjectArray = [];
+
+  for (let key in hashTable) {
+    characterObjectArray.push({ character: key, num: hashTable[key] });
+  }
+  return characterObjectArray;
+};
 
 //specialized merge array for objects
 const merge = (arr1, arr2) => {
@@ -75,64 +78,56 @@ const mergeSort = arr => {
 
 //tally characters until max number of characters is reached
 const tallyCharacters = (sortedArray, maxChars) => {
-    const finalChars = [];
-    let charTally = 0;
-    let i = 0;
-    // while current character's count + current character tally is under max number of characters
-    while(sortedArray[i].num +charTally < maxChars){
-        //add number to tally
-        charTally = charTally + sortedArray[i].num;
-        //add character to array 
-        finalChars.push(sortedArray[i].character)
-        //move to next character object
-        i++;
-    }
+  const finalChars = [];
+  let charTally = 0;
+  let i = 0;
+  // while current character's count + current character tally is under max number of characters
+  while (sortedArray[i].num + charTally < maxChars) {
+    //add number to tally
+    charTally = charTally + sortedArray[i].num;
+    //add character to array
+    finalChars.push(sortedArray[i].character);
+    //move to next character object
+    i++;
+  }
 
-    // push leftover chars to array
-    let leftoverChars = []
-    while(i < sortedArray.length){
-        leftoverChars.push(sortedArray[i].character)
-        i++;
-    }
+  // push leftover chars to array
+  let leftoverChars = [];
+  while (i < sortedArray.length) {
+    leftoverChars.push(sortedArray[i].character);
+    i++;
+  }
 
-    return { 
-        numOfCharsRemoved: charTally, 
-        uniqueChars: finalChars,
-        numOfUniqueCharsRemoved: finalChars.length,
-        leftoverChars: leftoverChars,
-        leftoverNumOfChars: maxChars - charTally + 50,
-    };
-}
+  return {
+    numOfCharsRemoved: charTally,
+    uniqueChars: finalChars,
+    numOfUniqueCharsRemoved: finalChars.length,
+    leftoverChars: leftoverChars,
+    leftoverNumOfChars: maxChars - charTally + 50
+  };
+};
 
+const findMaxChars = str => {
+  //split into array of characters, remove spaces
+  const letterArr = stringToArray(str);
 
+  //get length of array(num of Chars)
+  const paraLength = letterArr.length;
 
+  //get max num of characters that can be removed
+  const maxChars = paraLength - 50;
 
-const findMaxChars = (str) => {
-    //split into array of characters, remove spaces 
-    const letterArr = stringToArray(str);
-    
-    //get length of array(num of Chars)
-    const paraLength = letterArr.length
+  //create a hash table with the character and number of times it appears in the paragraph
+  const characterTable = createHashTable(letterArr);
 
-    //get max num of characters that can be removed
-    const maxChars = paraLength - 50;
+  //split object into array of objects by character for sorting
+  const characterObjectArray = createObjectArray(characterTable);
 
-    //create a hash table with the character and number of times it appears in the paragraph
-    const characterTable = createHashTable(letterArr);
+  //sort object from least char appearances to greatest
+  const sorted = mergeSort(characterObjectArray);
 
-    //split object into array of objects by character for sorting
-    const characterObjectArray = createObjectArray(characterTable);
-
-    //sort object from least char appearances to greatest
-    const sorted = mergeSort(characterObjectArray);
-    
-    //tally characters
-    return talliedCharacters = tallyCharacters(sorted, maxChars);
-
-
-}
+  //tally characters
+  return (talliedCharacters = tallyCharacters(sorted, maxChars));
+};
 
 console.log(findMaxChars(testStr));
-
-
-
