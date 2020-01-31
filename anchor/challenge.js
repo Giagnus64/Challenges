35809a0fbbd1c4255ba03e86dbe47ffc42c8e6f3
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-01-31 12:33:22 -0500

    Implemented merge sort in anchor challenge

--- anchor/challenge.js
+++ anchor/challenge.js
@@ -31,15 +31,16 @@ for (let key in hashTable){
 
 console.log(characterObjectArray)
 
-// //sort array of objects from least to most
-// const sortingFunc = (a, b) => {
-//     if(a.num > b.num){
-//         return 1
-//     } else{
-//         return -1
-//     }
-// }
-// const sorted = hashTable.sort(sortingFunc)
+//sort array of objects from least to most
+const sortingFunc = (a, b) => {
+    if(a.num > b.num){
+        return 1
+    } else{
+        return -1
+    }
+}
+
+// const sortedCharObjArray = characterObjectArray.sort(sortingFunc)
 
 // // if character count + current character tally is under max number of characters, add character to array and add number to tally
 // const finalChars = [];
@@ -53,6 +54,58 @@ console.log(characterObjectArray)
 // })
 // console.log(finalChars)
 
+
+
+
+
+
+
+
+
+//specialized merge array for objects
+const merge = (arr1, arr2) => {
+  //establish a final sorted array to sort the elements, and a pointer for each array
+  let sortedArray = [];
+  let firstPointer = 0;
+  let secondPointer = 0;
+  //while the first pointer and the second pointer have not reached the end of their respective arrays
+  while (firstPointer < arr1.length && secondPointer < arr2.length) {
+    //add lesser number to array, and move pointer
+    if (arr2[secondPointer].num >= arr1[firstPointer].num) {
+      sortedArray.push(arr1[firstPointer]);
+      firstPointer++;
+    } else {
+      sortedArray.push(arr2[secondPointer]);
+      secondPointer++;
+    }
+  }
+  //after arrays have been exhausted, add the rest of the numbers from whatever array's pointer has not reached gone through the array
+  while (firstPointer < arr1.length) {
+    sortedArray.push(arr1[firstPointer]);
+    firstPointer++;
+  }
+  while (secondPointer < arr2.length) {
+    sortedArray.push(arr2[secondPointer]);
+    secondPointer++;
+  }
+  return sortedArray;
+};
+//merge sort to sort object array
+const mergeSort = arr => {
+  //if array is SORTED(length is 0 or 1), return array
+  if (arr.length <= 1) return arr;
+  //find midpoint
+  let midpoint = Math.floor(arr.length / 2);
+  //split array by midpoint and merge sort the split arrays
+  let arr1 = mergeSort(arr.slice(0, midpoint));
+
+  let arr2 = mergeSort(arr.slice(midpoint));
+  //return the merged arrays
+  return merge(arr1, arr2);
+};
+
+const sorted = mergeSort(characterObjectArray);
+console.log(sorted);
 // const testStr = "We're an independent division within Spotify, with the power to move fast, plus the resources of the world's most forward-thinking audio company. Come help us build the future of podcasting! If you want to jumpstart the process of talking to us about this role, here’s a little challenge: write a program that outputs the largest unique set of characters that can be removed from this paragraph without letting its length drop below 50."
 
 // //split array into array of characters
