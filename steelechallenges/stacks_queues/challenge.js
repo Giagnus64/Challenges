7525211aad4615f37b75a349cb096bedf065fab1
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-02-10 21:39:33 -0500

    Finished implementation of quq with linkedlist

--- steelechallenges/stacks_queues/challenge.js
+++ steelechallenges/stacks_queues/challenge.js
@@ -30,7 +30,7 @@
 // //[{ "Willow Rosenberg": "A" }, { "Xander Harris": "C" }]
 
 //Array implementation of a stack using shift/unshift
-
+/*
 
 stack.unshift({"Willow Rosenberg": "A"});
 console.log(stack);
@@ -58,7 +58,7 @@ const secondOut = stack.shift();
 //{"Cordelia Chase": "B+"}
 console.log(stack);
 //[ { 'Xander Harris': 'C' }, { 'Willow Rosenberg': 'A' } ]
-
+*/
 
 //linked list implementation of stack adding and removing to the beginning for constant time
 class Node {
@@ -86,14 +86,14 @@ class Stack {
           newNode.next = this.first;
           this.first = newNode;
         }
-        //add 1 to length
-        this.length++;
+        //add 1 to size
+        this.size++;
 
         return this;
     }
     pop(){
         //if stack is empty return false
-        if (this.length === 0) return false;
+        if (this.size === 0) return false;
         //get poppednode
         const poppedNode = this.first;
         //get new first (could be NULL if stack is length 1)
@@ -104,8 +104,8 @@ class Stack {
         }
         //assign new first
         this.first = newFirst;
-        //remove 1 from length
-        this.length--;
+        //remove 1 from size
+        this.size--;
         //return shiftednode
         return poppedNode;
         }
@@ -113,6 +113,7 @@ class Stack {
 
 //Array implementation of a queue using unshift and pop
 // QUEUE = First In First Out
+//NOT ideal
 
 const queue = [];
 /*
@@ -161,3 +162,76 @@ queue.shift();
 console.log(queue);
 // ["Picture_of_Biscuit.png"];
 */
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    //newnode goes to back of the line/end of the queue
+    enqueue(value){
+        const newNode = new Node(value);
+        //if queue is empty
+        if (this.size === 0) {
+          this.first = newNode;
+          this.last = newNode;
+          // add current first pointer to new first(new node), and make new node new first
+        } else {
+          this.last.next = newNode;
+          this.last = newNode;
+        }
+        //add 1 to size
+        this.size++;
+
+        return this;
+    }
+    // dequeue nodes off the front of the line
+    dequeue(){
+         //if queue is empty return false
+        if (this.size === 0) return false;
+        //get poppednode
+        const dequeuedNode = this.first;
+        //get new first (could be NULL if stack is length 1)
+        const newFirst = this.first.next;
+        //if newFirst is null, reassign last to newFirst(null)
+        if (!newFirst) {
+            this.last = newFirst;
+        }
+        //assign new first
+        this.first = newFirst;
+        //remove 1 from size
+        this.size--;
+        //return dequeuednode
+        return dequeuedNode;
+        }
+
+        log(){
+           let currentNode = this.first
+           let i = 0;
+           while(currentNode){
+               console.log(i, currentNode.value)
+               i++;
+               currentNode = currentNode.next
+           }
+        }
+ }
+
+ const printQueue = new Queue();
+
+ printQueue.enqueue("Data_Report.csv");
+ printQueue.log()
+ //["Resume.docx", "Data_Report.csv"]
+
+ printQueue.enqueue("Picture_of_Biscuit.png");
+ printQueue.log()
+ //["Resume.docx", "Data_Report.csv", "Picture_of_Biscuit.png"]
+
+ printQueue.dequeue();
+ //"Resume.docx"
+ printQueue.log()
+ //["Data_Report.csv","Picture_of_Biscuit.png"]
+
+ printQueue.dequeue();
+ //"Data_report.csv"
+ printQueue.log()
