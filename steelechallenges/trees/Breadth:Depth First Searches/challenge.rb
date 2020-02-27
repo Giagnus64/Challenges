83677e1b03db44d645f017e46a6c7786ac3723e5
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-02-26 20:49:41 -0500

    Added Ruby BFS implementation

--- steelechallenges/trees/Breadth:Depth First Searches/challenge.rb
+++ steelechallenges/trees/Breadth:Depth First Searches/challenge.rb
@@ -7,10 +7,46 @@ class QueueNode
     end
 end
 
-class Tree
-    attr_accessor :root
+class Queue 
+    attr_accessor :size, :first, :last
+
     def initialize()
-        @root = nil 
+      @first = nil
+      @last = nil 
+      @size = 0 
+    end
+
+    def enqueue(val)
+        new_node = QueueNode.new(val)
+
+        if(self.size == 0)
+            self.first = new_node
+            self.last = new_node
+        else
+            self.last.next = new_node
+            self.last = new_node
+        end
+
+        self.size +=1
+
+        return self
+    end
+
+    # take off first node of list
+    def dequeue
+        if(self.size == 0)
+            return false
+        end
+        dequeued_node = self.first
+        new_first = dequeued_node.next
+        if(!new_first)
+            self.last = new_first
+        end
+        dequeued_node.next = nil
+        self.first = new_first
+        self.size -= 1
+
+        return dequeued_node
     end
 end
 
@@ -22,11 +58,57 @@ class TreeNode
     end
 end 
 
-class Queue 
-    attr_accessor :size, :first, :last
+class Tree
+    attr_accessor :root
+
     def initialize()
-      @first = nil
-      @last = nil 
-      @size = nil  
+        @root = nil 
+    end
+
+    def traverseBFS()
+        if(!self.root)
+            return false
+        end
+        line = Queue.new()
+        tree_values = []
+        line.enqueue(self.root)
+        while(line.size != 0)
+          node_children = line.first.value.children
+          if(node_children.length != 0)
+            node_children.each do |child|
+            line.enqueue(child)
+            end
+          end
+          tree_values.push(line.first.value.value)
+          line.dequeue
+        end
+        return tree_values
+    end
+
+    def traverseDFS(type)
+        if(!self.root)
+            return false
+        end
+        tree_values = []
+        current = self.root
+
     end
 end
+
+
+
+
+
+test_tree = Tree.new()
+test_tree.root = TreeNode.new("H");
+test_tree.root.children.push(TreeNode.new("e"));
+test_tree.root.children.push(TreeNode.new("l"));
+test_tree.root.children[0].children.push(TreeNode.new("l"));
+test_tree.root.children[0].children.push(TreeNode.new("o"));
+test_tree.root.children[0].children.push(TreeNode.new("W"));
+test_tree.root.children[1].children.push(TreeNode.new("o"));
+test_tree.root.children[1].children.push(TreeNode.new("r"));
+test_tree.root.children[1].children.push(TreeNode.new("l"));
+test_tree.root.children[1].children.push(TreeNode.new("d"));
+
+puts test_tree.traverseBFS()
