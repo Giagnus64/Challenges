2f664fe7be5f2f6346e40e13b87df16a1e1d661f
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-03-25 16:45:48 -0400

    Added remove Vertex method

--- steelechallenges/graphs/challenge.js
+++ steelechallenges/graphs/challenge.js
@@ -19,6 +19,19 @@ class Graph{
         this.adjacencyList[vertex1].push(vertex2)
         this.adjacencyList[vertex2].push(vertex1)  
     }
+    removeEdge(vertex1, vertex2){
+        let newEdges = this.adjacencyList[vertex1].filter((ele => ele !== vertex2))
+        this.adjacencyList[vertex1] = newEdges;
+        newEdges = this.adjacencyList[vertex2].filter((ele => ele !== vertex1))
+        this.adjacencyList[vertex2] = newEdges;
+    }
+
+    removeVertex(vertexName){
+        this.adjacencyList[vertexName].forEach((edge) => {
+            this.removeEdge(vertexName, edge)
+        })
+        delete this.adjacencyList[vertexName]
+    }
 }
 
 let testGraph = new Graph();
@@ -27,6 +40,14 @@ testGraph.addVertex("Jack");
 testGraph.addVertex("Jill");
 testGraph.addEdge("Jack", "Jill")
 testGraph.addEdge("Jack", "Ryan")
+testGraph.addEdge("Jack", "Sally")
+testGraph.addEdge("Jill", "Sally")
+testGraph.addEdge("Ben", "Bob")
+testGraph.addEdge("Ben", "Jill")
+testGraph.addEdge("Ben", "Sally")
+testGraph.addEdge("Bob", "Sally")
+
+testGraph.removeVertex("Sally")
 
 console.log(testGraph.adjacencyList)
 
