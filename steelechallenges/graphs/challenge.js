861a0ab31c8690550ace168e8ba8d9a03fe05080
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-03-25 16:30:22 -0400

    Added addEdge functionality

--- steelechallenges/graphs/challenge.js
+++ steelechallenges/graphs/challenge.js
@@ -1,15 +1,32 @@
+//undirected, unweighted
 class Graph{
     constructor(){
         this.adjacencyList = {}
     }
     addVertex(vertexName){
-        this.adjacencyList[vertexName] = [];
+        if(!this.adjacencyList[vertexName]){
+            this.adjacencyList[vertexName] = [];
+        }
+    }
+    addEdge(vertex1, vertex2){
+        //make sure vertexes exist
+        if(!this.adjacencyList[vertex1]){
+            this.addVertex(vertex1)
+        }
+        if(!this.adjacencyList[vertex2]){
+            this.addVertex(vertex2)
+        }
+        this.adjacencyList[vertex1].push(vertex2)
+        this.adjacencyList[vertex2].push(vertex1)  
     }
 }
 
 let testGraph = new Graph();
 
-testGraph.addVertex("smoothie")
+testGraph.addVertex("Jack");
+testGraph.addVertex("Jill");
+testGraph.addEdge("Jack", "Jill")
+testGraph.addEdge("Jack", "Ryan")
 
 console.log(testGraph.adjacencyList)
 
