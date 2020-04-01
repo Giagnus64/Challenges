847192208d5d126d197a16d3014327d4cb87d000
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-04-01 14:22:33 -0400

    add edge on ruby graph

--- steelechallenges/graphs/challenge.rb
+++ steelechallenges/graphs/challenge.rb
@@ -12,10 +12,20 @@ class Graph
     end
 
     def add_edge(vertex_1, vertex_2)
-        
+        if(!self.adjacency_list[vertex_1])
+            self.add_vertex(vertex_1)
+        end
+        if(!self.adjacency_list[vertex_2])
+            self.add_vertex(vertex_2)
+        end
+        self.adjacency_list[vertex_1].push(vertex_2)
+        self.adjacency_list[vertex_2].push(vertex_1)
     end
 end
 
 test_graph = Graph.new()
 test_graph.add_vertex("Jack");
+test_graph.add_edge("Jack", "Jill");
+
+
 puts test_graph.adjacency_list
